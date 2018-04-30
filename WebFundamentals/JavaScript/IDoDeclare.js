28caf5506d199f52093c67a46a6c3a8e814aac09
Kevin <stradtkt22@gmail.com>
2018-04-29 23:24:07 -0400

    Added IDoDeclare.js

---
+++ WebFundamentals/JavaScript/IDoDeclare.js
@@ -0,0 +1,22 @@
+/**
+ * Declare variables of the following types:
+4 Numbers
+3 Strings
+2 Booleans
+1 Undefined Variable
+ */
+
+var number1 = 10.20;
+var number2 = 1000;
+var number3 = 100;
+var number4 = 500;
+var string1 = "Hello World!";
+var string2 = "Kevin";
+var string3 = "Hello, how are you doing today?";
+var boolean1 = true;
+var boolean2 = false;
+var undefinedStatus = undefined;
+console.log('The font size is '+number1+'.'+'\n'+'The width is '+number2+'px.'+'\n'+'There are '+number3+' people in the class.'+'\n'+'The height is '+number4+'px.');
+console.log(string1+" My name is "+string2+'\n'+string3);
+console.log('My name is Kevin?'+'\n'+boolean1+'\n'+"My height is 7 foot?"+ '\n'+ boolean2);
+console.log('The item has a status of '+undefinedStatus)
---
+++ WebFundamentals/JavaScript/WhileYouWait.js
@@ -0,0 +1,1 @@
+var daysUntilBirthday = 200;
