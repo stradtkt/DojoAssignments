7456657e2cbcd7920666ed9c13fef769396a098c
Kevin <stradtkt22@gmail.com>
2018-05-06 18:52:57 -0400

    Added somedays() to date js algorithm file

--- AlgorithmBook/chapter-2/DateOnADesertedIsland.js
+++ AlgorithmBook/chapter-2/DateOnADesertedIsland.js
@@ -13,38 +13,110 @@ Having completed this epic saga, you are ready for additional concepts and chapt
  */
 
  //1
-function weekDayName(weekdayNum) {
-  switch(weekdayNum) {
-    case 1:
-      console.log("Sunday");
-      break;
-    case 2:
-      console.log('Monday');
-      break;
-    case 3:
-      console.log('Tuesday');
-      break;
-    case 4:
-      console.log('Wednesday');
-      break;
-    case 5:
-      console.log('Thursday');
-      break;
-    case 6:
-      console.log('Friday');
-      break;
-    case 7:
-      console.log('Saturday');
-      break;
-    default:
-      console.log('Invalid weekday number');
-  }
-}
-weekdayNum(7);
+// function weekDayName(weekdayNum) {
+//   switch(weekdayNum) {
+//     case 1:
+//       console.log("Sunday");
+//       break;
+//     case 2:
+//       console.log('Monday');
+//       break;
+//     case 3:
+//       console.log('Tuesday');
+//       break;
+//     case 4:
+//       console.log('Wednesday');
+//       break;
+//     case 5:
+//       console.log('Thursday');
+//       break;
+//     case 6:
+//       console.log('Friday');
+//       break;
+//     case 7:
+//       console.log('Saturday');
+//       break;
+//     default:
+//       console.log('Invalid weekday number');
+//   }
+// }
+// weekDayName(7);
 
-//2
+
+
+// //2
+// function weekDayName2(weekdayNum) {
+//   var con = weekdayNum % 7;
+//   switch(con) {
+//     case 0:
+//       console.log('Saturday');
+//       break;
+//     case 1: 
+//       console.log('Sunday');
+//       break;
+//     case 2:
+//       console.log('Monday');
+//       break;
+//     case 3: 
+//       console.log('Tuesday');
+//       break;
+//     case 4:
+//       console.log('Wednesday');
+//       break;
+//     case 5:
+//       console.log('Thursday');
+//       break;
+//     case 6:
+//       console.log('Friday');
+//       break;
+//   }
+// }
+// weekDayName2(1);
+
+
 
 //3
+function weekDayName2(weekdayNum) {
+  var con = weekdayNum % 7;
+
+  switch(con) {
+    case 1: 
+      console.log('Sunday');
+      return 1;
+    case 2:
+      console.log('Monday');
+      return 2;
+    case 3:
+      console.log('Tuesday');
+      return 3;
+    case 4:
+      console.log('Wednesday');
+      return 4;
+    case 5:
+      console.log('Thursday');
+      return 5;
+    case 6: 
+      console.log('Friday');
+      return 6;
+    case 7:
+      console.log('Saturday');
+      return 7;
+  }
+}
+
+function someDays() {
+  var num;
+  for(var i = 1; i <= 100; i++) {
+    num = weekDayName2(Math.floor(Math.random() * 365 + 1));
+    if(num == 1 || num == 0) {
+      console.log('Enjoy your day off!');
+    } else {
+      console.log('Work Hard!');
+    }
+  }
+}
+someDays();
+
 
 //4
 
