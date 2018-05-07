c064dbb1aa19150741cb27591998f016e5ea4ff1
Kevin <stradtkt22@gmail.com>
2018-05-07 17:01:12 -0400

    Added bootstrap project and finished basicFoundations.js file

--- WebFundamentals/JavaScript/BasicFoundation1.js
+++ WebFundamentals/JavaScript/BasicFoundation1.js
@@ -135,3 +135,18 @@ function swapValues(arr) {
 // swapValues([10,8,6,-2]);
 
 
+/**
+ * Number to String - Write a function that takes an array of numbers and replaces any negative values within the array with 
+ * the string 'Dojo'. For example if array = [-1,-3,2], your function will return ['Dojo','Dojo',2].
+ */
+
+function numberToString(arr) {
+  for(var i = 0; i < arr.length; i++) {
+    if(arr[i] < 0) {
+      arr[i] = 'Dojo';
+    }
+  }
+  console.log(arr);
+  return arr;
+}
+numberToString([-1,-2,1,1,-1,-2]);
