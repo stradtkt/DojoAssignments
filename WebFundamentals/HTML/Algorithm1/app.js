b76d4d5ced71cd649ab7cc42ff7f1cd87d810b84
Kevin <stradtkt22@gmail.com>
2018-05-10 13:14:35 -0400

    Added Algorithm 1 from the html folder

---
+++ WebFundamentals/HTML/Algorithm1/app.js
@@ -0,0 +1,41 @@
+// var x = [1,2,3,4,5,10]
+// for(var i=0; i<5;i++){   
+//    console.log(i);
+// }
+//output 0, 1, 2, 3, 4
+
+// var x = [1,2,3,4,5,10]
+// for(var i=0; i<5; i++)
+// {
+//    i = i + 1;  
+//    console.log(i);
+// }
+// output 1,3,5
+
+
+// var x = [1,2,3,4,5,10]
+// for(var i=0; i<5; i++)
+// {
+//    i = i + 3;
+//    console.log(i);
+// }
+
+//output 3,7
+
+// function y(num1, num2) {   
+//   return num1+num2;
+// }
+// console.log(y(2,3))
+// console.log(y(3,5))
+
+//output 5,8
+
+
+// function y(num1, num2) {
+//   console.log(num1);   
+//   return num1+num2;
+// }
+// console.log(y(2,3))
+// console.log(y(3,5))
+
+//output 2,5,3,8
