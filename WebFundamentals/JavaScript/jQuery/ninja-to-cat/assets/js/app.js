115581d72879a9758b527dbd1e38ae82ae58d92c
Kevin <stradtkt22@gmail.com>
2018-05-13 19:29:17 -0400

    Added jQuery ninja to cat

---
+++ WebFundamentals/JavaScript/jQuery/ninja-to-cat/assets/js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $('.pic0').on('click', function(e) {
+    e.preventDefault();
+    $(this).attr('src', 'assets/img/cat0.png');
+  });
+  $('.pic1').on('click', function(e) {
+    e.preventDefault();
+    $(this).attr('src', 'assets/img/cat1.png');
+  });
+  $('.pic2').on('click', function(e) {
+    e.preventDefault();
+    $(this).attr('src', 'assets/img/cat2.png');
+  });
+  $('.pic3').on('click', function(e) {
+    e.preventDefault();
+    $(this).attr('src', 'assets/img/cat3.png');
+  });
+  $('.pic4').on('click', function(e) {
+    e.preventDefault();
+    $(this).attr('src', 'assets/img/cat4.png');
+  });
+});
