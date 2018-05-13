58c51e8cef7485b8abf0a97e0bb4b1380e0a0355
Kevin <stradtkt22@gmail.com>
2018-05-13 17:03:34 -0400

    Added jQueryFunctions.js

--- WebFundamentals/JavaScript/jQuery/jQueryFunctions.js
+++ WebFundamentals/JavaScript/jQuery/jQueryFunctions.js
@@ -37,22 +37,55 @@ $(document).ready(function() {
 
 
   function form() {
-    var name = $('#name').val(),
-         age = $('#age').val(),
-         email = $('#email').val();
-
-         $('form').submit(function(e) {
-           e.preventDefault();
-           if(name) {
-             $('.content').html(name);
-           }
-           if(age) {
-             $('.content').html(age);
-           }
-           if(email) {
-             $('.content').html(email);
-           }
-         })
+    $('form').on('submit', function(e) {
+      e.preventDefault();
+      $('.content').html('<p>Content Appended</p>');
+      $('#subject').val("This is the subject content");
+      $('.content2').text('You thought I was going to let you post your own content?');
+    });
   }
   form();
+
+
+  $( ".weather" ).click( function() {
+    var value,
+      div = $( ".div" )[ 0 ];
+    switch ( $( ".weather" ).index( this ) ) {
+    case 0 :
+      value = jQuery.data( div, "weather" );
+      break;
+    case 1 :
+      jQuery.data( div, "weather", "sunny" );
+      value = "Stored!";
+      break;
+    case 2 :
+      jQuery.data( div, "weather", "rainy" );
+      value = "Stored!";
+      break;
+    case 3 :
+      jQuery.removeData( div, "weather" );
+      value = "Removed!";
+      break;
+    }
+    $( "span" ).text( "" + value );
+  });
+
+  var title = $('.attr p').attr('title');
+  $('.attr p').before("Title = "+title);
+
+
+  $('.toggleP').click(function(e) {
+    $('.toggle p').toggle(function() {
+      $(this).addClass('.blue');
+    }, function() {
+      $(this).removeClass('.blue');
+    });
+    e.preventDefault();
+  });
+
+  $('.show p').hide();
+  $('.showP').click(function(e) {
+    e.preventDefault();
+    $('.show p').show();
+  });
 });
