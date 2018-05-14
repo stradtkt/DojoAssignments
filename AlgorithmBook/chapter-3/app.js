048f3049ffbbc9d59adbf98840d8ee7994d54d15
Kevin <stradtkt22@gmail.com>
2018-05-13 21:46:44 -0400

    Added more to chapter-3 algorithms

--- AlgorithmBook/chapter-3/app.js
+++ AlgorithmBook/chapter-3/app.js
@@ -42,3 +42,38 @@ Given array, remove and return the value at the beginning of the array. Do this 
  }
 //  console.log(popFront([1,2,3,4,5]));
 // output [2,3,4,5]
+
+/**
+ *  Array: Remove At
+ * Given array and an index into array, remove and return the array value at that 
+ * index. Do this without using built-in array methods except pop(). Think of popFront(arr) as equivalent to removeAt(arr,0).
+ */
+
+function removeAt(arr, idx) {
+  var temp = arr[idx];
+  for(var i = idx; i < arr.length; i++) {
+    arr[i] = arr[i+1];
+  }
+  arr.length--;
+  console.log(arr);
+  return temp;
+}
+// console.log(removeAt([1,2,3,4,5], 2));
+
+/**
+ * Array: Swap Pairs
+ * Swap positions of successive pairs of values of given array. If length is odd, do not change the final element. For [1,2,3,4], return [2,1,4,3]. For example, 
+ * change input ["Brendan",true,42] to [true,"Brendan",42]. As with all array challenges, do this without using any built-in array methods.
+ */
+
+function swapPairs(arr) {
+  var limit;
+  arr.length % 2 != 0 ? limit = arr.length-1 : limit = arr.length;
+  for(var i = 0; i < limit; i+=2) {
+    var temp = arr[i];
+    arr[i] = arr[i+1];
+    arr[i+1] = temp;
+  }
+  return arr;
+}
+// console.log(swapPairs([1,2,3,4,5,6]));
