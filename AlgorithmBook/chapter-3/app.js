efdfa03fbed085ef1cee257bc2f4b8467833712d
Kevin <stradtkt22@gmail.com>
2018-05-14 18:10:05 -0400

    Added more to algorithm chapter 3 and the whats that pokemon

--- AlgorithmBook/chapter-3/app.js
+++ AlgorithmBook/chapter-3/app.js
@@ -11,6 +11,7 @@ function pushFront(arr, val) {
   return arr;
 }
 // console.log(pushFront([1,2,3,4,5,6],22));
+// output [22,1,2,3,4,5,6]
 
 /**
  * Array: Insert At
@@ -27,6 +28,7 @@ Do this without using built-in array methods. You can think of pushFront(arr,val
    return arr;
  }
 //  console.log(insertAt([1,2,3,4,5,6],2,55));
+ // output [1,2,55,3,4,5,6]
 
 /**
  * Array: Pop Front
@@ -59,6 +61,7 @@ function removeAt(arr, idx) {
   return temp;
 }
 // console.log(removeAt([1,2,3,4,5], 2));
+//output [1,2,4,5], 3
 
 /**
  * Array: Swap Pairs
@@ -77,3 +80,166 @@ function swapPairs(arr) {
   return arr;
 }
 // console.log(swapPairs([1,2,3,4,5,6]));
+// output [2,1,4,3,6,5]
+
+/**
+ * Array: Remove Duplicates
+ * Sara is looking to hire an awesome web developer 
+ * and has received applications from various sources.
+ * Her assistant alphabetized them but noticed some duplicates. 
+ * Given a sorted array, remove duplicate values. Because array elements are already in order, 
+ * all duplicate values will be grouped together. 
+ * As with all these array challenges, do this without using any built-in array methods.
+ */
+
+function removeDuplicates(arr) {
+  for(var i = 0; i < arr.length; i++) {
+    if(arr[i+1] == arr[i]) {
+      for(var j = i; j < arr.length; j++) {
+        arr[j] = arr[j+1];
+      }
+      arr.length--;
+      i--;
+    }
+  }
+  return arr;
+}
+
+// console.log(removeDuplicates([1,1,2,2,3,3,4,4,5,5,6,6]));
+//output [1,2,3,4,5,6]
+
+
+/**
+ * Array: Min to Front
+ * Given an array of comparable values, move the lowest element to array’s front,
+ * shifting backward any elements previously ahead of it. Do not otherwise change the array’s order. 
+ * Given [4,2,1,3,5], change it to [1,4,2,3,5] and return it. As always, do this without using built-in functions. 
+ */
+function removeAt2(arr, idx) {
+  for(var i = idx; i < arr.length; i++) {
+    arr[i] = arr[i+1];
+  }
+  arr.length--;
+  return arr;
+}
+function insertAt2(arr, idx, val) {
+  for(var i = arr.length - 1; i >= idx; i--) {
+    arr[i+1] = arr[i];
+  }
+  arr[idx] = val;
+  return arr;
+}
+
+function minToFront(arr) {
+  var min = arr[0];
+  var m;
+  for(var i = 1; i < arr.length; i++) {
+    if(arr[i] < min) {
+      min = arr[i];
+      m = i;
+    }
+  }
+  arr = removeAt2(arr, m);
+  return insertAt2(arr, 0, min);
+}
+// console.log(minToFront([4,5,1,2,3,4]));
+// output [1,4,5,2,3,4]
+
+/**
+ * Array: Reverse
+ * Given a numerical array, reverse the order of values, in-place. The reversed array should have the same length, 
+ * with existing elements moved to other indices so that order of elements is reversed. Working ‘in-place’ means that you cannot use a second array – 
+ * move values within the array that you are given. As always, do not use built-in array functions such as splice().
+ */
+
+function reverseArray(arr) {
+  var left = 0;
+  var right = arr.length-1;
+  var temp;
+  while(left < right) {
+    temp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temp;
+    left++;
+    right--;
+  }
+  return arr;
+}
+
+// console.log(reverseArray([1,2,3,4,5,6]));
+// output [6,5,4,3,2,1]
+
+function reverseArray2(arr) {
+  var temp;
+  for(var i = 0; i < arr.length/2; i++) {
+    temp = arr[i];
+    arr[i] = arr[arr.length-1-i];
+    arr[arr.length-1-i] = temp;
+  }
+  return arr;
+}
+// console.log(reverseArray2([6,5,4,3,2,1]));
+// output [1,2,3,4,5,6]
+
+function reverseArray3(arr) {
+  var temp;
+  for(var i = 0; i < arr.length-i; i++) {
+    temp = arr[i];
+    arr[i] = arr[arr.length-1-i];
+    arr[arr.length-1-i] = temp;
+  }
+  return arr;
+}
+// console.log(reverseArray3([10,9,8,7,6,5,4,3,2,1]));
+// output [1,2,3,4,5,6,7,8,9,10]
+
+/**
+ * Array: Rotate
+ * Implement rotateArr(arr, shiftBy) that accepts array and offset. Shift arr’s values to the right by that amount. 
+ * Wrap-around’ any values that shift off array’s end to the other side, so that no data is lost. 
+ * Operate in-place: given ([1,2,3],1), change the array to [3,1,2]. Don’t use built-in functions. 
+ * Second: allow negative shiftBy (shift L, not R). 
+ * Third: minimize memory usage. With no new array, handle arrays/shiftBys in the millions. 
+ * Fourth: minimize the touches of each element. 
+ */
+
+function rotateArray(arr, shiftBy) {
+  var temp;
+  for(var i = 1; i <= shiftBy; i++) {
+    temp = arr[arr.length-1];
+    for(var j = arr.length-1; j > 0; j--) {
+      arr[j] = arr[j-1];
+    }
+    arr[0] = temp;
+  }
+  return arr;
+}
+// console.log(rotateArray([1,2,3,4],1));
+// output [4,1,2,3]
+
+/**
+ * Array: Filter Range
+ * Alan is good at breaking secret codes. One method is to eliminate values that lie within a specific known range. 
+ * Given arr and values min and max, retain only the array values between min and max. 
+ * Work in-place: return the array you are given, with values in original order. No built-in array functions. 
+ */
+
+function removeAt3(arr, idx) {
+  for(var i = idx; i < arr.length; i++) {
+    arr[i] = arr[i+1];
+  }
+  arr.length--;
+  return arr;
+}
+function filterRange(arr, min, max) {
+  for(var i = 0; i < arr.length; i++) {
+    if(arr[i] < min || arr[i] > max) {
+      removeAt3(arr, i);
+      i--;
+    }
+  }
+  return arr;
+}
+// console.log(filterRange([1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,6], 3,4));
+// output [3,4,3,4]
+
--- WebFundamentals/JavaScript/Ajax/whats-that-pokemon/assets/js/app.js
+++ WebFundamentals/JavaScript/Ajax/whats-that-pokemon/assets/js/app.js
@@ -1,14 +1,29 @@
-$(document).ready(function() {
-  function displayPokemon() {
-    for(var i = 1; i <= 40; i++) {
-      $('.images').append(`<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${i}.png" />`)
+$(document).ready(function(){
+    var id, img, types, pokemonName, pokemonHeight, pokemonWeight;
+    for (var i = 1; i <= 50; i++){
+        $(".images").append('<img id="' + i + '" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/' + i + '.png">');
     }
-  }
-  displayPokemon();
-
-  function clickImage() {
-    $('.images').click(function(e) {
-      e.preventDefault();
+    $('img').click(function(){
+        id = $(this).attr('id');
+        img = '<img id="' + id + '" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/' + id + '.png">';
+        $.get("https://pokeapi.co/api/v2/pokemon/" + id + "/", function(res) {
+            var pokemonName = "";
+            pokemonName += "<h1 class='text-center'>" + res.name + "</h1>";
+            $('.name').html(pokemonName);
+            $('.picture').html(img);
+            types = "<h3 class='text-center'>Types</h3>";
+            types += "<ul>";
+            for(var i = 0; i < res.types.length; i++) {
+              types += "<li>" + res.types[i]["type"].name + "</li>";
+            }
+            types += "</ul>";
+            $(".types").html(types);
+            pokemonHeight = "<h3 class='text-center'>Height</h3>";
+            pokemonHeight += res.height;
+            $('.height').html(pokemonHeight);
+            pokemonWeight = "<h3 class='text-center'>Weight</h3>";
+            pokemonWeight += res.weight;
+            $('.weight').html(pokemonWeight);
+        }, 'json');
     });
-  }
 });
