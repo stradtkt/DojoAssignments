f99bfb6dc9f0bf0a649e3e8459d194c060a31dae
Kevin <stradtkt22@gmail.com>
2018-05-15 01:31:57 -0400

    yourBirthday.js added

---
+++ WebFundamentals/AlgorithmApp/yourBirthday.js
@@ -0,0 +1,8 @@
+function yourBirthday(num1, num2) {
+  if((num1 == 8 && num2 == 25) || (num1 == 25 && num2 == 8)) {
+    console.log("How did you know?");
+  } else {
+    console.log("Just another day...");
+  }
+}
+yourBirthday(8,25);
