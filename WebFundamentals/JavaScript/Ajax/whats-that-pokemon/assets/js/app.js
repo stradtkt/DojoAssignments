ddd5b8f5ab5a5ab0e57c4249c3600cd930117fda
Kevin <stradtkt22@gmail.com>
2018-05-14 21:46:30 -0400

    Final Commit on the what's that pokemon

--- WebFundamentals/JavaScript/Ajax/whats-that-pokemon/assets/js/app.js
+++ WebFundamentals/JavaScript/Ajax/whats-that-pokemon/assets/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var id, img, types, pokemonName, pokemonHeight, pokemonWeight;
+    var id;
     for (var i = 1; i <= 50; i++){
         $(".images").append('<img id="' + i + '" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/' + i + '.png">');
     }
@@ -7,23 +7,21 @@ $(document).ready(function(){
         id = $(this).attr('id');
         img = '<img id="' + id + '" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/' + id + '.png">';
         $.get("https://pokeapi.co/api/v2/pokemon/" + id + "/", function(res) {
-            var pokemonName = "";
-            pokemonName += "<h1 class='text-center'>" + res.name + "</h1>";
-            $('.name').html(pokemonName);
-            $('.picture').html(img);
-            types = "<h3 class='text-center'>Types</h3>";
-            types += "<ul>";
+            var html = "";
+            html += "<h3>" + res.name + "</h3>";
+            html += img;
+            html += "<h4>Types</h4>";
+            html += "<ul>";
             for(var i = 0; i < res.types.length; i++) {
-              types += "<li>" + res.types[i]["type"].name + "</li>";
+              html += "<li>" + res.types[i]["type"].name + "</li>";
             }
-            types += "</ul>";
-            $(".types").html(types);
-            pokemonHeight = "<h3 class='text-center'>Height</h3>";
-            pokemonHeight += res.height;
-            $('.height').html(pokemonHeight);
-            pokemonWeight = "<h3 class='text-center'>Weight</h3>";
-            pokemonWeight += res.weight;
-            $('.weight').html(pokemonWeight);
+            html += "</ul>";
+            html += "<h4>Height</h4>";
+            html += "<p>"+ res.height +"</p>";
+            html += "<h4>Weight</h4>";
+            html += "<p>"+ res.weight +"</p>";
+            html += "</ul>";
+            $('.content').html(html);
         }, 'json');
     });
 });
