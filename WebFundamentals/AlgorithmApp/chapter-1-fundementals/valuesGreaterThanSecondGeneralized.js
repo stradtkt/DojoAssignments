184c7c0d3a1e33e1c362a10c3af86c3fa868c250
Kevin <stradtkt22@gmail.com>
2018-05-15 09:43:48 -0400

    Changes to generalized in chapter-1

--- WebFundamentals/AlgorithmApp/chapter-1-fundementals/valuesGreaterThanSecondGeneralized.js
+++ WebFundamentals/AlgorithmApp/chapter-1-fundementals/valuesGreaterThanSecondGeneralized.js
@@ -1,11 +1,17 @@
 function valuesGreaterThanSecondGeneralized(arr) {
   var newArr = [];
+  var count = 0;
+  if(arr.length < 2) {
+    return "There are not enough items in the array";
+  }
   for(var i = 0; i < arr.length; i++) {
     if(arr[i] > arr[1]) {
       newArr.push(arr[i]);
+      count++;
     }
   }
+  console.log(count);
   return newArr;
 }
 console.log(valuesGreaterThanSecondGeneralized([1,3,5,7,9,13]));
-// output [5,7,9,13]
+// output [5,7,9,13], 4
