606ca95b5498c1b79be68a9f816aa68edd35c8c6
Kevin <stradtkt22@gmail.com>
2018-05-15 14:22:37 -0400

    added more to printLowReturnHigh.js

--- WebFundamentals/AlgorithmApp/chapter-1-fundementals/printLowReturnHigh.js
+++ WebFundamentals/AlgorithmApp/chapter-1-fundementals/printLowReturnHigh.js
@@ -1,5 +1,18 @@
-function printLowReturnHigh(num1, num2) {
-  console.log(num1);
-  return num2;
+function printLowReturnHigh(array) {
+  var low = array[0];
+  var high = array[0];
+  for(var i = 1; i < array.length; i++) {
+    if(array[i] < low) {
+      low = array[i];
+    }
+    if(array[i] > high) {
+      high = array[i];
+    }
+  }
+  console.log("The low is: " + low);
+  console.log('The high is: '+ high); 
+  return "The high is "+ high;
 }
-printLowReturnHigh(1,2);
+printLowReturnHigh([1,2,3,4,5,6,7,8,9]);
+
+//output The low is: 1, The high is 9
