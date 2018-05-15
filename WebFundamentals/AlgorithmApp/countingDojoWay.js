37992edc6f4b279f689613b6ed7229741339a369
Kevin <stradtkt22@gmail.com>
2018-05-15 01:50:08 -0400

    countingDojoWay.js added

---
+++ WebFundamentals/AlgorithmApp/countingDojoWay.js
@@ -0,0 +1,12 @@
+function countingDojoWay() {
+  for(var i = 0; i <= 100; i++) {
+    if(i % 5 === 0) {
+      console.log("Coding");
+    }
+    if(i % 10 === 0) {
+      console.log(" Dojo");
+    }
+    console.log(i);
+  }
+}
+countingDojoWay();
